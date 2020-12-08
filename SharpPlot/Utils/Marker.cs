f52f1b3f92b4e2a9702bfa62efcf99853209b24a
NonoMalpi <antoniomalpicamorales@gmail.com>
2020-12-08 10:05:25 +0100

    #4 feat: Add Marker enum for point type

---
+++ SharpPlot/Utils/Marker.cs
@@ -0,0 +1,21 @@
+namespace SharpPlot.Utils
+{
+    public enum Marker
+    {
+        Plus=1,
+        Cross=2,
+        Asterisk=3,
+        BlankSquare=4,
+        ColoredSquare=5,
+        BlankCircle=6,
+        ColoredCircle=7,
+        BlankTriangle=8,
+        ColoredTriangle=9,
+        BlankInvertedTriangle=10,
+        ColoredInvertedTriangle=11,
+        BlankDiamond=12,
+        ColoredDiamond=13,
+        BlankPentagon=14,
+        ColoredPentagon=15
+    }
+}
