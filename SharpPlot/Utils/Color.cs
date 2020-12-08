7cee23fea171894734def7ebb96a2f443f18970c
NonoMalpi <antoniomalpicamorales@gmail.com>
2020-12-08 10:07:01 +0100

    #6 feat: Add basic Color enum

---
+++ SharpPlot/Utils/Color.cs
@@ -0,0 +1,16 @@
+namespace SharpPlot.Utils
+{
+    public enum Color
+    {
+        Black=0,
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Orange,
+        Purple,
+        Brown,
+        White,
+        Grey
+    }
+}
