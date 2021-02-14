85d3ba29426e82b32ee7d6d69f1fd7a61b2a640c
NonoMalpi <antoniomalpicamorales@gmail.com>
2021-02-14 22:21:00 +0100

    #16 feat: Add DataPoints test

--- SharpPlot.Unittest/Canvas/DataPoints.cs
+++ SharpPlot.Unittest/Canvas/DataPoints.cs
@@ -1,10 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics;
 using NUnit.Framework;
+using SharpPlot.Canvas;
 
 namespace SharpPlot.UnitTest.Canvas
 {
     [TestFixture]
     public class TestDataPoints
     {
-        //TODO: Add DataPoints tests
+        private DataPoints _dataPoints1D;
+        private DataPoints _dataPoints2D;
+        private DataPoints _dataPoints3D;
+        private DataPoints _dataPoints4D;
+        private List<double> _x = Generate.LinearSpaced(10, 0, 10).ToList();
+        private List<double> _y = Generate.LinearSpaced(10, 0, 10).Select(e => e * 2).ToList();
+        private List<double> _z = Generate.LinearSpaced(10, 0, 10).Select(e => e * 4).ToList();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dataPoints1D = new DataPoints(x: _x);
+            _dataPoints2D = new DataPoints(x: _x, y: _y);
+            _dataPoints3D = new DataPoints(x: _x, y: _y, z: _z);
+            _dataPoints4D = new DataPoints(x1: _x, x2: _y, y1: _z, y2: _z);
+        }
+
+        [Test]
+        public void Test1DConstructor()
+        {
+            var expectedArray = new double[1][];
+            expectedArray[0] = _x.ToArray();
+            Assert.AreEqual(expectedArray, _dataPoints1D.Array);
+        } 
+        
+        [Test]
+        public void Test2DConstructor()
+        {
+            var expectedArray = new double[2][];
+            expectedArray[0] = _x.ToArray();
+            expectedArray[1] = _y.ToArray();
+            Assert.AreEqual(expectedArray, _dataPoints2D.Array);
+        }
+        
+        [Test]
+        public void Test3DConstructor()
+        {
+            var expectedArray = new double[3][];
+            expectedArray[0] = _x.ToArray();
+            expectedArray[1] = _y.ToArray();
+            expectedArray[2] = _z.ToArray();
+            Assert.AreEqual(expectedArray, _dataPoints3D.Array);
+        }
+        
+        [Test]
+        public void Test4DConstructor()
+        {
+            var expectedArray = new double[4][];
+            expectedArray[0] = _x.ToArray();
+            expectedArray[1] = _y.ToArray();
+            expectedArray[2] = _z.ToArray();
+            expectedArray[3] = _z.ToArray();
+            Assert.AreEqual(expectedArray, _dataPoints4D.Array);
+        } 
+
+        [Test]
+        public void TestDimProperty()
+        {
+            Assert.AreEqual(1, _dataPoints1D.Dim);
+            Assert.AreEqual(2, _dataPoints2D.Dim);
+            Assert.AreEqual(3, _dataPoints3D.Dim);
+            Assert.AreEqual(4, _dataPoints4D.Dim);
+        }
+
+        [Test]
+        public void Test1DStreamPoints()
+        {
+            List<string> expectedStream = new List<string>();
+            expectedStream = _x.Select((e) => $"{e}").ToList();
+            expectedStream.Add("e" + Environment.NewLine);
+            Assert.AreEqual(expectedStream, _dataPoints1D.StreamPoints());
+        }
+        
+        [Test]
+        public void Test2DStreamPoints()
+        {
+            List<string> expectedStream = new List<string>();
+            expectedStream = _x.Select((e, idx) => $"{e} {_y[idx]}").ToList();
+            expectedStream.Add("e" + Environment.NewLine);
+            Assert.AreEqual(expectedStream, _dataPoints2D.StreamPoints());
+        }
+        
+        [Test]
+        public void Test3DStreamPoints()
+        {
+            List<string> expectedStream = new List<string>();
+            expectedStream = _x.Select((e, idx) => $"{e} {_y[idx]} {_z[idx]}").ToList();
+            expectedStream.Add("e" + Environment.NewLine);
+            Assert.AreEqual(expectedStream, _dataPoints3D.StreamPoints());
+        }
+        
+        [Test]
+        public void Test4DStreamPoints()
+        {
+            List<string> expectedStream = new List<string>();
+            expectedStream = _x.Select((e, idx) => $"{e} {_y[idx]} {_z[idx]} {_z[idx]}").ToList();
+            expectedStream.Add("e" + Environment.NewLine);
+            Assert.AreEqual(expectedStream, _dataPoints4D.StreamPoints());
+        }
+
+
     }
 }
